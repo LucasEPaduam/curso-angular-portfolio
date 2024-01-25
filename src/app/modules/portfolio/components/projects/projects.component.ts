1874f8dc58cb9f7398847f9c9a7ba3c3f9d2c441
Lucas Paduam <lucasepaduam@gmail.com>
2024-01-25 19:04:44 -0300

    ProjectsComponent

---
+++ src/app/modules/portfolio/components/projects/projects.component.ts
@@ -0,0 +1,43 @@
+import { Component, signal } from '@angular/core';
+import { IProjects } from '../../interface/IProjects.interface';
+
+@Component({
+  selector: 'app-projects',
+  standalone: true,
+  imports: [],
+  templateUrl: './projects.component.html',
+  styleUrl: './projects.component.scss'
+})
+export class ProjectsComponent {
+  public arrayProjects = signal<IProjects[]>([
+    {
+      src: 'assets/img/projects/vfull.png',
+      alt: '',
+      title: "Fullstack",
+      width: '100px',
+      height: '51px',
+      description: '',
+      links: [
+        {
+            name: 'Conheça',
+            href: '',
+        }
+    ]
+    },
+    {
+      src: 'assets/img/Foto_Lucas_Perfil.jpg',
+      alt: '',
+      title: "Fullstack",
+      width: '100px',
+      height: '51px',
+      description: '',
+      links: [
+        {
+            name: 'Conheça',
+            href: '',
+        }
+    ]
+    },
+  ]);
+
+}
